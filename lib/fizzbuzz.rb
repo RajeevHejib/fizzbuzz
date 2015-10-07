9247fdbddbaab556850cf04d877f49a2d7dae239
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-10-07 18:01:35 +0100

    added private methods

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,22 +1,29 @@
-def fizzbuzz(numb)
-  return "fizzbuzz" if divisible_by_15?(numb)
-  return "fizz" if calc(numb, 3)
-  return "buzz" if calc(numb, 5)
-  numb
-end
+class Game
+  def fizzbuzz(numb)
+    return "fizzbuzz" if divisible_by_15?(numb)
+    return "fizz" if divisible_by_3?(numb)
+    return "buzz" if divisible_by_5?(numb)
+    numb
+  end
 
-def divisible_by_15?(numb)
-  calc(numb, 15)
-end
+  private
+  def divisible_by_15?(numb)
+    calc(numb, 15)
+  end
 
-def divisible_by_3?(numb)
-  calc(numb, 3)
-end
+  private
+  def divisible_by_3?(numb)
+    calc(numb, 3)
+  end
 
-def divisible_by_5?(numb)
-  calc(numb, 5)
-end
+  private
+  def divisible_by_5?(numb)
+    calc(numb, 5)
+  end
 
-def calc(numb, divisor)
- numb % divisor == 0
+  private
+  def calc(numb, divisor)
+    numb % divisor == 0
+  end
+
 end
--- spec/fizzbugg_spec.rb
+++ spec/fizzbugg_spec.rb
@@ -1,24 +1,24 @@
 require './lib/fizzbuzz.rb'
 
 describe "fizzbuzz"  do
-
+  play = Game.new
   it "returns fizz when passed 3" do
-    expect(fizzbuzz(3)).to eq("fizz")
+    expect(play.fizzbuzz(3)).to eq("fizz")
   end
 
   it "returns buzz when passed 5" do
-    expect(fizzbuzz(5)).to eq("buzz")
+    expect(play.fizzbuzz(5)).to eq("buzz")
   end
 
   it "returns fizzbuzz when passed 15" do
-    expect(fizzbuzz(15)).to eq("fizzbuzz")
+    expect(play.fizzbuzz(15)).to eq("fizzbuzz")
   end
 
   it "returns 2 when passed 2" do
-    expect(fizzbuzz(2)).to eq(2)
+    expect(play.fizzbuzz(2)).to eq(2)
   end
 
   it "returns buzz when passed 20" do
-    expect(fizzbuzz(20)).to eq("buzz")
+    expect(play.fizzbuzz(20)).to eq("buzz")
   end
 end
