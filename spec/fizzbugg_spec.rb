b561bf7072bc4f678ed56de7494451195c97a85b
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-11-20 11:37:48 +0000

    some minor changes

--- spec/fizzbugg_spec.rb
+++ spec/fizzbugg_spec.rb
@@ -1,24 +1,24 @@
 require './lib/fizzbuzz.rb'
 
 describe "fizzbuzz"  do
-  play = Game.new
+  game = Game.new
   it "returns fizz when passed 3" do
-    expect(play.fizzbuzz(3)).to eq("fizz")
+    expect(game.fizzbuzz(3)).to eq("fizz")
   end
 
   it "returns buzz when passed 5" do
-    expect(play.fizzbuzz(5)).to eq("buzz")
+    expect(game.fizzbuzz(5)).to eq("buzz")
   end
 
   it "returns fizzbuzz when passed 15" do
-    expect(play.fizzbuzz(15)).to eq("fizzbuzz")
+    expect(game.fizzbuzz(15)).to eq("fizzbuzz")
   end
 
   it "returns 2 when passed 2" do
-    expect(play.fizzbuzz(2)).to eq(2)
+    expect(game.fizzbuzz(2)).to eq(2)
   end
 
   it "returns buzz when passed 20" do
-    expect(play.fizzbuzz(20)).to eq("buzz")
+    expect(game.fizzbuzz(20)).to eq("buzz")
   end
 end
